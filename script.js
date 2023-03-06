2905d63bc29eae67e4a1a7ebf4566b29d5fab879
MELISSA MAZIR <melissamazir07@gmail.com>
2023-03-06 18:18:57 +0100

    finished the js for the menu button

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-const humberger = document.querySelector(".humberger-bottun");
-const navbar = document.querySelector(".navigation-bar");
+const humberger = document.querySelector('.humberger-bottun');
+const navbar = document.querySelector('.navigation-bar');
 
-humberger.addEventListener("click", () => {
-    humberger.classList.toggle("active");
-    navbar.classList.toggle("active");
-})
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-    humberger.classList.remove("active");
-    navbar.classList.remove("active");
-}))
+humberger.onclick = function () {
+  humberger.classList.toggle('active');
+  navbar.classList.toggle('active');
+};
+navbar.onclick = function () {
+  humberger.classList.remove('active');
+  navbar.classList.remove('active');
+};
+
+// humberger.addEventListener("click", () => {
+//     humberger.classList.toggle("active");
+//     navbar.classList.toggle("active");
+// })
+// document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+//     humberger.classList.remove("active");
+//     navbar.classList.remove("active");
+// }))
