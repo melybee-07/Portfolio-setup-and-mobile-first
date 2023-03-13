aca9e8e5363e803b8e9f91466e3af569b8efcf84
MELISSA MAZIR <melissamazir07@gmail.com>
2023-03-13 15:02:12 +0100

    fixing the linters errors

--- script.js
+++ script.js
@@ -1,25 +1,20 @@
-let Humberger = document.querySelector('.humberger-bottun');
-let Navbar = document.querySelector('.navigation-bar');
+const Humberger = document.querySelector('.humberger-bottun');
+const Navbar = document.querySelector('.navigation-bar');
 
 // script to activate the humberger button once clicked.
 
-function humbergerfunction(){
+function humbergerfunction() {
   Humberger.classList.toggle('active');
   Navbar.classList.toggle('active');
 }
 
 Humberger.addEventListener('click', humbergerfunction);
 
-
-
 // Script to activate the nav bar once clicked
 
-function navbarfunction(){
+function navbarfunction() {
   Humberger.classList.remove('active');
   Navbar.classList.remove('active');
 }
 
 Navbar.addEventListener('click', navbarfunction);
-
-
-
